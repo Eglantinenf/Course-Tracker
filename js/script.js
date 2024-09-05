591b25c69bec7d6383c605f824ed730663dca6e3
Nastaran F <145607070+Eglantinenf@users.noreply.github.com>
2024-09-05 22:07:13 +0330

    script.js

--- js/script.js
+++ js/script.js
@@ -35,21 +35,45 @@ function markCompleted(index) {
   }
 }
 
+function deleteCourse(index) {
+  courses.splice(index, 1);
+  localStorage.setItem("courses", JSON.stringify(courses));
+  renderCourses();
+}
+
+function formatTime(time) {
+  if (time < 60) {
+    return `${time} min`;
+  } else {
+    const hours = Math.floor(time / 60);
+    const minutes = time % 60;
+    return minutes === 0 ? `${hours} hr` : `${hours} hr ${minutes} min`;
+  }
+}
+
 function renderCourses() {
   const courseList = document.getElementById("courseList");
   courseList.innerHTML = "";
   totalRemaining = 0;
+  totalSpent = 0;
+
   courses.forEach((course, index) => {
     const listItem = document.createElement("li");
     listItem.className = course.completed
       ? "completed border-b border-gray-300 py-2"
       : "border-b border-gray-200 py-2";
-    listItem.innerHTML = `  
-                    <div class="flex justify-between items-center">  
-                        <span class="text-lg font-medium">${course.title} - Total Lectures: ${course.totalTime}</span>  
-                        <button onclick="markCompleted(${index})" class="bg-green-500 text-white rounded-lg p-1 ml-2 hover:bg-green-600 transition duration-200">Complete Lecture</button>  
-                    </div>  
-                `;
+
+    const formattedTime = formatTime(course.totalTime);
+
+    listItem.innerHTML = `
+      <div class="flex justify-between items-center">
+        <span class="text-lg font-medium">${course.title} - Total Lectures: ${formattedTime}</span>
+        <div>
+          <button onclick="markCompleted(${index})" class="bg-green-500 text-white rounded-lg p-1 ml-2 hover:bg-green-600 transition duration-200">Complete Lecture</button>
+          <button onclick="deleteCourse(${index})" class="bg-red-500 text-white rounded-lg p-1 ml-2 hover:bg-red-600 transition duration-200">Delete</button>
+        </div>
+      </div>
+    `;
     courseList.appendChild(listItem);
   });
 
@@ -57,8 +81,15 @@ function renderCourses() {
     (sum, course) => sum + (course.completed ? 0 : course.totalTime),
     0
   );
-  document.getElementById("totalSpent").innerText = totalSpent;
-  document.getElementById("totalRemaining").innerText = totalRemaining;
+
+  totalSpent = courses.reduce(
+    (sum, course) => sum + (course.completed ? course.totalTime : 0),
+    0
+  );
+
+  document.getElementById("totalSpent").innerText = formatTime(totalSpent);
+  document.getElementById("totalRemaining").innerText =
+    formatTime(totalRemaining);
 }
 
 renderCourses();
